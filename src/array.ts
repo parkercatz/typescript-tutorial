2c65210081a6b3aa95072996b6d4f20ba03c4ae5
ParkerCatz <tksm.webdesign09@gmail.com>
2021-04-27 21:23:13 +0900

    feat: array型の定義

---
+++ src/array.ts
@@ -0,0 +1,15 @@
+export {}
+
+let numbers: number[] = [1, 2, 3, 4, 5]
+
+let numbers2: Array<number> = [1, 2, 3, 4]
+let strings2: Array<string> = ['Tokyo', 'Osaka', 'Kyoto']
+
+let strings: string[] = ['Typescript', 'Javascript', 'coffeescript']
+
+let nijigenhairetu: number[][] = [
+  [10, 20],
+  [100, 300],
+]
+
+let hairetu: (string | number | boolean)[] = [1, false, 'japan']
