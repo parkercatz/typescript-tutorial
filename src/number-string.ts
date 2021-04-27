bb014bab761e841b7d06c1c4bb74d118cb94e007
ParkerCatz <tksm.webdesign09@gmail.com>
2021-04-27 21:13:36 +0900

    feat: number & stringの型定義

---
+++ src/number-string.ts
@@ -0,0 +1,15 @@
+export {}
+
+let year: number = 1991
+// year = false
+
+console.log(year)
+
+let age: number = 0x2b
+
+console.log(age)
+
+let name: string = 'Taro'
+// name = 111
+
+console.log(name)
