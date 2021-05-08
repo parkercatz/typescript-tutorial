b77d8b79a4f43f46aa3af105f26e33af690fc789
ParkerCatz <tksm.webdesign09@gmail.com>
2021-05-08 09:57:29 +0900

    feat: interfaceの定義

---
+++ src/interface.ts
@@ -0,0 +1,16 @@
+export {}
+
+type ObjectType = {
+  name: string
+  year: number
+}
+
+interface ObjectInterface {
+  name: string
+  year: number
+}
+
+let object: ObjectInterface = {
+  name: 'Tokyo',
+  year: 2020,
+}
